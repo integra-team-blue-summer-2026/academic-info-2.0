d5954c0b5b0cfe150993ff28a5ba30329c739336
AioaneClaudia <claudiavalentina2005@yahoo.com>
2026-08-13 11:14:56 +0300

    #21: Implement frontend interface for managing courses

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
-import {Routes} from '@angular/router';
-import {Home} from './features/home/home';
+import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { CourseList } from './features/courses/course-list/course-list';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'courses',
+    component: CourseList,
   }
 ];
